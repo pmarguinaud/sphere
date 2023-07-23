c577d19bd8ee3168672ad46ee6d7d10c5bbdf51f
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-07-23 12:10:32 +0200

    Render texture + sphere

--- main.cc
+++ main.cc
@@ -97,6 +97,7 @@ void main ()
 
   void render () const
   {
+    glUseProgram (programID);
     glViewport (0, 0, width, height);
     glDrawElementsInstanced (GL_TRIANGLES, 3 * nt, GL_UNSIGNED_INT, ind, Nx * Ny);
   }
@@ -168,6 +169,113 @@ int main (int argc, char * argv[])
       if (glfwWindowShouldClose (window) != 0)  
         break;
     }   
+
+  int Nj = atoi (argv[1]);
+  int np; 
+  float * xyz;
+  unsigned int nt; 
+  unsigned int * ind;
+  const int width = 1024, height = 1024;
+  int w, h;
+  unsigned char * rgb = NULL;
+
+  bmp ("Whole_world_-_land_and_oceans_8000.bmp", &rgb, &w, &h);
+
+  gensphere (Nj, &np, &xyz, &nt, &ind);
+
+  GLuint VertexArrayID;
+  GLuint vertexbuffer, colorbuffer, elementbuffer;
+
+  glGenVertexArrays (1, &VertexArrayID);
+  glBindVertexArray (VertexArrayID);
+
+  glGenBuffers (1, &vertexbuffer);
+  glBindBuffer (GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData (GL_ARRAY_BUFFER, 3 * np * sizeof (float), xyz, GL_STATIC_DRAW);
+  glEnableVertexAttribArray (0); 
+  glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL); 
+  
+  glGenBuffers (1, &elementbuffer);
+  glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+  glBufferData (GL_ELEMENT_ARRAY_BUFFER, 3 * nt * sizeof (unsigned int), ind , GL_STATIC_DRAW);
+
+  GLuint programID = shader 
+(
+R"CODE(
+#version 330 core
+
+in vec4 fragmentColor;
+
+out vec4 color;
+
+void main()
+{
+  color.r = fragmentColor.r;
+  color.g = fragmentColor.g;
+  color.b = fragmentColor.b;
+  color.a = fragmentColor.a;
+}
+)CODE",
+R"CODE(
+#version 330 core
+
+layout(location = 0) in vec3 vertexPos;
+
+out vec4 fragmentColor;
+
+uniform mat4 MVP;
+
+uniform sampler2D texture;
+
+void main()
+{
+  float lon = (atan (vertexPos.y, vertexPos.x) / 3.1415926 + 1.0) * 0.5;
+  float lat = asin (vertexPos.z) / 3.1415926 + 0.5;
+  gl_Position =  MVP * vec4 (vertexPos, 1);
+
+  vec4 col = texture2D (texture, vec2 (lon, lat));
+  fragmentColor.r = col.r;
+  fragmentColor.g = col.g;
+  fragmentColor.b = col.b;
+  fragmentColor.a = 1.;
+}
+)CODE");
+
+  glUseProgram (programID);
+
+  glm::mat4 Projection = glm::perspective (glm::radians (20.0f), 1.0f / 1.0f, 0.1f, 100.0f);
+  glm::mat4 View       = glm::lookAt (glm::vec3 (6.0f,0.0f,0.0f), glm::vec3 (0,0,0), glm::vec3 (0,0,1));
+  glm::mat4 Model      = glm::mat4 (1.0f);
+
+  glm::mat4 MVP = Projection * View * Model; 
+
+  glUniformMatrix4fv (glGetUniformLocation (programID, "MVP"), 1, GL_FALSE, &MVP[0][0]);
+
+  unsigned int texture;
+  glGenTextures (1, &texture);
+  glBindTexture (GL_TEXTURE_2D, texture); 
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexImage2D (GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, rgb);
+
+  while (1) 
+    {   
+      glUseProgram (programID);
+      glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      glDrawElements (GL_TRIANGLES, 3 * nt, GL_UNSIGNED_INT, NULL);
+
+      glfwSwapBuffers (window);
+      glfwPollEvents (); 
+  
+      if (glfwGetKey (window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        break;
+      if (glfwWindowShouldClose (window) != 0)  
+        break;
+    }   
+
 
 
   glfwTerminate ();
